9ac50d7670ad462904ff3170c45c8f9f06169f26
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-09-25 12:24:29 -0700

    Add first test class: OrderTest to create the order instance

---
+++ StoreVendors.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresVendors.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresVendors.Tests
+{
+    [TestClass]
+    public class OrderTest : IDisposable
+    {
+        public void Dispose()
+        {
+            Order.ClearAll();
+        }
+
+        [TestMethod]
+        public void OrderConstructor_CreatesInstanceOfOrder_Order()
+        {
+            Order newOrder = new Order("test", "test", "test", "test");
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+
+    }
+} 
