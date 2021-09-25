f084abdf11586a9b02c0e9b0509abd408dc0708d
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-09-25 11:57:19 -0700

    add constructor and obj method to Order.cs

--- StoreVendors/Controllers/OrdersController.cs
+++ StoreVendors/Controllers/OrdersController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using PierresVendors.Models;
 using System.Collections.Generic;
 
-namespace PierresVendors.Controllers
+namespace StoreVendors.Controllers
 {
 public class OrdersController : Controller
 {
---
+++ StoreVendors/Models/Order.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace StoreVendors.Models
+{
+	public class Order
+	{
+	public string Name { get; set; }
+	public string Date { get; set; }
+	public string Desc { get; set; }
+	public string Cost { get; set; }
+	public int Id { get; }
+	private static List<Order> _instances = New List<Order> { };
+
+	public Order(string name, string date, string desc, string cost)
+	{
+		Name = name;
+		Date = date;
+		Desc = desc;
+		Cost = cost;
+		_instances.Add(this);
+		Id = _instances.Count;
+	}	
+
+	public static List<Order> GetAll()
+	{
+		return _instances;
+	}
+
+	public static void ClearAll()
+	{
+		_instances.Clear();
+	}
+
+	public static Order Find(int searchId)
+		{
+		return _instances[searchId - 1];
+		}
+	}
+}
