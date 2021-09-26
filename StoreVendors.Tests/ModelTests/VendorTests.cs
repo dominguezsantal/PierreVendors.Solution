6c177f0a66ff3df595e0b68111ea62f0fecfaab5
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-09-25 19:05:11 -0700

    test #12 passing now

--- StoreVendors.Tests/ModelTests/VendorTests.cs
+++ StoreVendors.Tests/ModelTests/VendorTests.cs
@@ -43,13 +43,13 @@ public class VendorTest : IDisposable
 	[TestMethod]
     public void GetAll_ReturnsAllVendorObjects_VendorList()
     {
-      string name01 = "Jeff Bezos";
-      string name02 = "Bill Gates";
-      Vendor newVendor1 = new Vendor(name01);
-      Vendor newVendor2 = new Vendor(name02);
-      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
-      List<Vendor> result = Vendor.GetAll();
-      CollectionAssert.AreEqual(newOctollionaire, result);
+    string name01 = "Jeff Bezos";
+    string name02 = "Bill Gates";
+    Vendor newVendor1 = new Vendor(name01);
+    Vendor newVendor2 = new Vendor(name02);
+    List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+    List<Vendor> result = Vendor.GetAll();
+    CollectionAssert.AreEqual(newList, result);
     }
 
 
