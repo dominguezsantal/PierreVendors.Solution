f304cb9bbe3aafcbb42bfe7d048baf2f428e000a
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-09-25 19:02:25 -0700

    test #12 good fail. newList puposefully changed to newOctollionaire (line 52)

--- StoreVendors.Tests/ModelTests/VendorTests.cs
+++ StoreVendors.Tests/ModelTests/VendorTests.cs
@@ -40,6 +40,18 @@ public class VendorTest : IDisposable
     Assert.AreEqual(1, result);
     }
 
+	[TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "Jeff Bezos";
+      string name02 = "Bill Gates";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newOctollionaire, result);
+    }
+
 
 
     
